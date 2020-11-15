263b827d5d723b5797697caf70a6bae209ceac65
王磊 <610213949@qq.com>
2020-11-16 02:54:50 +0800

    项目初始化

---
+++ src/views/home/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='home-container'>
+    home
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  props: {},
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+
+  computed: {},
+
+  created () { },
+
+  mounted () { },
+
+  methods: {},
+
+  watch: {}
+
+}
+
+</script>
+<style lang='less' scoped>
+.home-container {
+}
+</style>
---
+++ src/views/nine/nine.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='nine-container'>
+   nine
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'nine',
+  props: {},
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+
+  computed: {},
+
+  created () { },
+
+  mounted () { },
+
+  methods: {},
+
+  watch: {}
+
+}
+
+</script>
+<style lang='less' scoped>
+.nine-container {
+}
+</style>
