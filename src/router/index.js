da24abda850f8715afceaff0ec26cae7a90510e5
王磊 <610213949@qq.com>
2020-11-28 13:46:11 +0800

    调整路由模块设置路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { Dialog } from 'vant'
+import store from '@/store'
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return routerPush.call(this, location).catch(error => error)
+}
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/login',
-    name: 'Login',
-    component: () => import('../views/login/login.vue')
+    name: 'login',
+    component: () => import('../views/login/login.vue'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/',
@@ -16,39 +23,85 @@ const routes = [
       {
         path: '', // 默认子路由
         name: 'home',
-        component: () => import('@/views/home/home')
+        component: () => import('@/views/home/home'),
+        meta: { requiresAuth: false }
       },
+      /* 九块九 */
       {
         path: '/nine',
         name: 'nine',
-        component: () => import('@/views/nine/nine')
+        component: () => import('@/views/nine/nine'),
+        meta: { requiresAuth: false }
       },
+      /* 分类 */
       {
         path: '/menu',
         name: 'menu',
-        component: () => import('@/views/menu/menu')
+        component: () => import('@/views/menu/menu'),
+        meta: { requiresAuth: false }
       },
+      /* 收藏 */
       {
         path: '/collection',
         name: 'collection',
-        component: () => import('@/views/collection/collection')
+        component: () => import('@/views/collection/collection'),
+        meta: { requiresAuth: true }
       },
+      /* 我的 */
       {
         path: '/my',
         name: 'my',
-        component: () => import('@/views/my/my')
+        component: () => import('@/views/my/my'),
+        meta: { requiresAuth: false }
       }
     ]
   },
+  /* 商品详情页 */
   {
     path: '/goods/:goodsId/:id',
     name: 'Goods',
-    component: () => import('@/views/goods/goods')
+    component: () => import('@/views/goods/goods'),
+    meta: { requiresAuth: false }
+  },
+  /* 搜索 */
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('@/views/search/search'),
+    meta: { requiresAuth: false }
   }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    // 如果已经登录 直接放行
+    if (store.state.user) {
+      return next()
+    }
+    // 没有登录，提示是否登录
+    Dialog.confirm({
+      title: '访问提示',
+      message: '该功能需要登录才能访问，确认登录吗'
+    })
+      .then(() => {
+        // on confirm
+        router.replace({
+          name: 'login',
+          query: {
+            redirect: router.currentRoute.fullPath
+          }
+        })
+      })
+      .catch(() => {
+        // on cancel 中断路由导航
+        next(false)
+      })
+  } else {
+    // 不需要登录状态的页面，直接放行
+    next()
+  }
+})
 export default router
---
+++ src/views/search/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class='search-container'>
+    search
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'search',
+  props: {},
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+
+  computed: {},
+
+  created () { },
+
+  mounted () { },
+
+  methods: {},
+
+  watch: {}
+
+}
+
+</script>
+<style lang='less' scoped>
+.search-container {
+}
+</style>
